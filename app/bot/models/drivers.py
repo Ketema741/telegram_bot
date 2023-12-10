b73d1563e391c4f06533a97c048957f6de009a89
Ketema Girma <ketema.girma@a2sv.org>
2023-12-10 20:53:03 +0300

    add driver models

---
+++ app/bot/models/drivers.py
@@ -0,0 +1,9 @@
+class Driver:
+  def __init__(self, id, tg_id, full_name, phone, role, car_model, license_plate):
+    self.id = id
+    self.tg_id = tg_id
+    self.full_name = full_name
+    self.phone = phone
+    self.role = role
+    self.license_plate = license_plate
+    self.car_model = car_model
