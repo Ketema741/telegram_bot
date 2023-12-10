d7ea5c913f3a34ae7810d8eea040cea2aa60c7ff
Ketema Girma <ketema.girma@a2sv.org>
2023-12-10 20:51:55 +0300

    add passenger model

---
+++ app/bot/models/passenger.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, id, tg_id, full_name, phone, role):
+        self.id = id
+        self.tg_id = tg_id
+        self.full_name = full_name
+        self.phone = phone
+        self.role = role
